8e321e9d821b72ea0495cec5b386e30c8900f2ba
wushidiguo <wukai1207@gmail.com>
2022-12-08 13:57:03 +0800

    hello!

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ iface = gr.Interface(
     fn=demo, 
     inputs="image", 
     outputs="text",
-    title="Hello Lottery",
-    description="彩票OCR项目，通过神经网络识别彩票信息，给出中奖结果。目前支持体彩超级大乐透和福彩双色球，支持单式、复式、胆拖玩法。",
+    title="Hello! Lottery",
+    description="彩票OCR项目，通过神经网络识别彩票信息，给出中奖结果。目前支持体彩超级大乐透和福彩双色球，支持单式、复式、胆拖玩法。https://github.com/wushidiguo/hello-lottery",
     allow_flagging="never"
     )
 
