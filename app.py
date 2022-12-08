d3a67337d5a09e48834a707d8c846e903eac8a5c
wushidiguo <wukai1207@gmail.com>
2022-12-08 12:33:45 +0800

    servername

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ iface = gr.Interface(
     outputs="text",
     title="Hello Lottery",
     description="彩票OCR项目，通过神经网络识别彩票信息，给出中奖结果。目前支持体彩超级大乐透和福彩双色球，支持单式、复式、胆拖玩法。",
-    allow_flagging="never",
-    server_name="0.0.0.0"
+    allow_flagging="never"
     )
 
-iface.launch()
+iface.launch(server_name='0.0.0.0')
